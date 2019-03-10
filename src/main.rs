58fd9c8b5bdc9009add30db354c7861eaa929da0
Nathaniel Waisbrot <nathaniel@waisbrot.net>
2019-03-10 14:23:36 -0400

    demo of hashing without reading file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate postgres;
+extern crate crypto;
 
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
 use postgres::{Connection, TlsMode};
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
 
 struct File {
     hash: String,
@@ -17,7 +20,7 @@ impl File {
 }
 
 // one possible implementation of walking a directory only visiting files
-fn visit_dirs(dir: &Path, visitor: fn(&DirEntry) -> io::Result<()>) -> io::Result<()> {
+fn visit_dirs(dir: &Path, visitor: &Fn(&DirEntry) -> io::Result<()>) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
@@ -32,17 +35,29 @@ fn visit_dirs(dir: &Path, visitor: fn(&DirEntry) -> io::Result<()>) -> io::Resul
     Ok(())
 }
 
-fn print_dir(dir: &DirEntry) -> io::Result<()> {
-    println!("{:?}", dir.path());
-    Ok(())
+fn hash_from_path(path: &Path) -> io::Result<String> {
+    let mut hasher = Sha3::sha3_256();
+    let data = format!("{}", path.display());
+    hasher.input_str(&data);
+    Ok(hasher.result_str())
 }
 
 fn main() -> io::Result<()> {
     let connection = Connection::connect("postgres://media:media@tularemia.local", TlsMode::None)?;
     let root = Path::new("/Users/waisbrot/git/scan-to-postgres");
-    visit_dirs(root, print_dir)?;
-    let fake_file = File::new(String::from("Fake"), root);
-    let updates = connection.execute("INSERT INTO paths (hash, path) VALUES ($1, $2)", &[&fake_file.hash, &fake_file.path])?;
-    println!("Inserted {} rows", updates);
+    let print_dir = |dir: &DirEntry| -> io::Result<()> {
+        let path = dir.path();
+        let path = path.as_path();
+        let fake_file = File::new(hash_from_path(path)?, path);
+        let updates = connection.execute("INSERT INTO paths (hash, path) VALUES ($1, $2)", &[&fake_file.hash, &fake_file.path]);
+        match updates {
+            Ok(_) => { Ok(()) },
+            Err(e) => { println!("Error: {}", &e); Ok(()) }
+        }
+    };
+    visit_dirs(root, &print_dir)?;
+    //let fake_file = File::new(String::from("Fake"), root);
+    //let updates = connection.execute("INSERT INTO paths (hash, path) VALUES ($1, $2)", &[&fake_file.hash, &fake_file.path])?;
+    //println!("Inserted {} rows", updates);
     Ok(())
 }
